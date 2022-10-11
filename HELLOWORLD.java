43ab6bc5ae3c43bfe233c87976c5790e19c681e4
vijay kumar chowdary <vijaychowdary1437@gmail.com>
2022-10-11 13:52:49 +0530

    'hello

---
+++ HELLOWORLD.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package bredon.com;
+
+/**
+ * @author Hp
+ *
+ */
+public class HELLOWORLD {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to JAVA");
+
+	}
+
+}
